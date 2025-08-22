d8670d29fda35e7bd4561c37db431376e543f97f
Vortex <vortexsnail12@gmail.com>
2025-08-22 04:10:33 -0700

    WIP director reworks (notekeeping, for now)

--- Assets/WaveDirector.cs
+++ Assets/WaveDirector.cs
@@ -1,165 +1,47 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
 public static class WaveDirector
 {
-    #region legacy wave spawner
+    public class WaveModifiers
+    {
+        public GameObject WaveSpecialBonusEnemy { get; set; } = null;
+        public float EnemyScaling { get; set; } = 1.0f;
+        public float CreditGatherScaling { get; set; } = 1.0f;
+        public float InitialAmbush { get; set; } = 0.0f;
+        public void Reset()
+        {
+            WaveSpecialBonusEnemy = null;
+            EnemyScaling = CreditGatherScaling = 1;
+            InitialAmbush = 0.0f;
+        }
+        public void CloneValues(WaveModifiers other)
+        {
+            WaveSpecialBonusEnemy = other.WaveSpecialBonusEnemy;
+            EnemyScaling = other.EnemyScaling;
+            CreditGatherScaling = other.CreditGatherScaling;
+            InitialAmbush = other.InitialAmbush;
+        }
+    }
+    public static List<GameObject> PossibleEnemies()
+    {
+        List<GameObject> result = new(EnemyPool);
+        if (TemporaryModifiers.WaveSpecialBonusEnemy != null)
+            result.Add(TemporaryModifiers.WaveSpecialBonusEnemy);
+        return result;
+    }
+    public static readonly List<GameObject> EnemyPool = new();
+    public static readonly WaveModifiers PermanentModifiers = new();
+    public static readonly WaveModifiers TemporaryModifiers = new(); //Permanent modifiers, but with bonuses applied after cloning values
     public static float PointsSpent = 0;
     private static float PointTimer = 0;
-    private static float SoapTimer, DuckTimer, FlamingoTimer, MadLadTimer;
-    private static float minXBound = -30, maxXBound = 30, minYBound = -20, maxYBound = 20;
-    private static float bathBombTimer = 0;
-    private static bool RunOnce = true;
     public static int Point
     {
         get => UIManager.score;
         set => UIManager.score = value;
-    }
-    public static bool InsideBathtub(Vector2 pos)
-    {
-        pos.x *= 0.3f;
-        return pos.magnitude <= 20f;
-    }
-    public static bool CanSpawnPower()
-    {
-        return PointsSpent < Point && Utils.RandFloat(1.0f) < 0.25f;
-    }
-    private static float GetSpawnTime(float max, float min, float minimumThreshhold, float maxThreshold)
-    {
-        if (Point < minimumThreshhold)
-        {
-            return -1;
-        }
-        float dist = maxThreshold - minimumThreshhold;
-        float percent = (Point - minimumThreshhold) / dist;
-        return Mathf.Lerp(max, min, percent);
-    }
-    private static void EnemySpawning(ref float SpawnTimer, float SpawnTimerSpeedScaling, float respawnTime, GameObject Enemy)
-    {
-        if (SpawnTimer > respawnTime && respawnTime > 0)
-        {
-            if (TrySpawnEnemy(Enemy))
-                SpawnTimer = Utils.RandFloat(-0.5f, 0.5f) * respawnTime;
-        }
-        else
-        {
-            SpawnTimer += Time.deltaTime * SpawnTimerSpeedScaling;
-        }
-    }
-    private static bool TrySpawnEnemy(GameObject Enemy)
-    {
-        Vector2 stuff = Player.Position + new Vector2(Random.Range(minXBound, maxXBound), Random.Range(minYBound, maxYBound));
-        if ((stuff - Player.Position).magnitude < 25)
-        {
-            stuff -= Player.Position;
-            stuff = Player.Position + stuff.normalized * 25;
-        }
-        int att = 0;
-        while (!InsideBathtub(stuff))
-        {
-            stuff = Player.Position + new Vector2(Random.Range(minXBound, maxXBound), Random.Range(minYBound, maxYBound));
-            stuff *= 1f - (att / 100f);
-            if ((stuff - Player.Position).magnitude < 25)
-            {
-                stuff -= Player.Position;
-                stuff = Player.Position + stuff.normalized * 25;
-            }
-            if (++att > 100)
-            {
-                Debug.Log("Fail to spawn");
-                return false;
-            }
-        }
-        GameObject.Instantiate(Enemy, stuff, Quaternion.identity);
-        return true;
-    }
-    public static int GetBathBombType()
-    {
-        float[] weights = new float[] { 2, 1, 0.1f, 0, 0, 0, 0 };
-        if (Point < 200)
-        {
-            if (Point > 100)
-                weights = new float[] { 2, 2, 1, 0.2f, 0, 0, 0 };
-        }
-        else if (Point < 400)
-            weights = new float[] { 1, 2, 2, 1, 0.2f, 0.1f, 0 };
-        else if (Point < 700)
-            weights = new float[] { 0.1f, 1, 2, 2, 1, 0.1f, 0 };
-        else if (Point < 1000)
-            weights = new float[] { 0.1f, 1, 2, 2, 2, 1, 0.1f };
-        else if (Point < 1400)
-            weights = new float[] { 0.1f, 0.2f, 1, 2, 2, 2, 1 };
-        else if (Point < 2000)
-            weights = new float[] { 0.1f, 0.2f, 0.3f, 1, 2, 2, 1 };
-        else if (Point < 3000)
-            weights = new float[] { 0.1f, 0.2f, 0.3f, 1, 2, 3, 2 };
-        else
-            weights = new float[] { 0.1f, 0.2f, 0.3f, 1, 2, 3, 7 };
-        float total = 0f;
-        for (int i = 0; i < weights.Length; i++)
-        {
-            total += weights[i];
-        }
-        //Debug.Log("t: " + total);
-        float rand = Utils.RandFloat(total);
-        //Debug.Log("rand: " + rand);
-        for (int i = 0; i < weights.Length; i++)
-        {
-            if (rand < weights[i])
-            {
-                ///Debug.Log(rand + ": " + i);
-                return i;
-            }
-            else
-                rand -= weights[i];
-        }
-        return 0;
-    }
-    public static void SpawnBathBomb()
-    {
-        Vector2 randPos = new Vector2(Utils.RandFloat(-12, 12), Utils.RandFloat(-7, 7));
-        Vector2 predictedPosition = Player.Position + randPos;
-        int att = 0;
-        while (!InsideBathtub(predictedPosition))
-        {
-            predictedPosition = Player.Position + new Vector2(Utils.RandFloat(-12, 12), Utils.RandFloat(-7, 7));
-            if (++att > 100)
-            {
-                Debug.Log("Fail to spawn");
-                break;
-            }
-        }
-        Projectile.NewProjectile<BathBomb>(Player.Position + new Vector2(randPos.x, 30), Vector2.zero, Player.Position.y + randPos.y, GetBathBombType());
-    }
-    public static void LegacyUpdate()
-    {
-        if (RunOnce)
-        {
-            Vector2 spawnPos = new Vector2(Utils.RandFloat(24), 0).RotatedBy(Utils.RandFloat(6.284f));
-            PowerUp.Spawn<Choice>(spawnPos);
-            for (int i = 0; i < 10; i++)
-            {
-                TrySpawnEnemy(EnemyID.OldDuck);
-            }
-            RunOnce = false;
-        }
-        //if(Input.GetKeyDown(KeyCode.T))
-        //{
-        //    Point += 100;
-        //}
-        bathBombTimer -= Time.deltaTime;
-        if (bathBombTimer < 0)
-        {
-            SpawnBathBomb();
-            float minTime = GetSpawnTime(6, 1, 0, 5000);
-            float maxTime = GetSpawnTime(8, 2, 0, 10000);
-            bathBombTimer = Utils.RandFloat(minTime, maxTime); //5 to 8 seconds for another bath bomb
-        }
-        EnemySpawning(ref DuckTimer, 1f, GetSpawnTime(10, 5, 0, 1000), EnemyID.OldDuck);
-        EnemySpawning(ref SoapTimer, 1f, GetSpawnTime(20, 7, 80, 1100), EnemyID.OldSoap);
-        EnemySpawning(ref FlamingoTimer, 1f, GetSpawnTime(30, 20, 300, 1800), EnemyID.OldFlamingo);
-        EnemySpawning(ref MadLadTimer, 1f, GetSpawnTime(90, 30, 1000, 10000), EnemyID.OldLeonard);
     }
     public static void PointUpdate()
     {
@@ -174,7 +56,6 @@ public static class WaveDirector
             PointTimer--;
         }
     }
-    #endregion
     public static void Update()
     {
         if (!Main.WavesUnleashed)
@@ -185,8 +66,8 @@ public static class WaveDirector
     public static int MaxCards = 6;
     public static float Credits = 0, CreditsSpent = 0;
     public static float PlayRecoil;
-    public static List<WaveCard> Deck = new List<WaveCard>();
-    public static List<WaveCard> Board = new List<WaveCard>();
+    public static readonly List<WaveCard> Deck = new();
+    public static readonly List<WaveCard> Board = new();
     public static int CardsPlayed = 0;
     public static int WaveNum = 1;
     public static float WaveMult = 1.0f;
@@ -199,8 +80,11 @@ public static class WaveDirector
         CreditsSpent = 0;
         Deck.Clear();
         Board.Clear();
-        PointsSpent = PointTimer = PityPowersSpawned = SoapTimer = DuckTimer = FlamingoTimer = MadLadTimer = bathBombTimer = TotalPowersSpawned = 0;
-        RunOnce = true;
+        PointsSpent = PointTimer = PityPowersSpawned = TotalPowersSpawned = 0;
+
+        EnemyPool.Clear();
+        PermanentModifiers.Reset();
+        TemporaryModifiers.CloneValues(PermanentModifiers);
     }
     public static void FixedUpdate()
     {
@@ -323,6 +207,51 @@ public static class WaveDirector
                 t.SetComplete();
             }
         }
+
+        #region wave card updates
+        //---- Start of resolve ----
+
+        //Run previous wave card rewards (on completion rewards)
+        // NOT YET IMPLEMENTED
+
+        //Resolve previous wave cards (on resolutions)
+        // NOT YET IMPLEMENTED
+
+        //Update enemy pool and reset temporary modifiers
+        if (PermanentModifiers.WaveSpecialBonusEnemy != null) //If it was permanent, add the previous wave's special enemy to the enemy pool
+        {
+            EnemyPool.Add(PermanentModifiers.WaveSpecialBonusEnemy);
+            PermanentModifiers.WaveSpecialBonusEnemy = null;
+        }
+
+        //Draw new wave cards
+        // NOT YET IMPLEMENTED
+
+        //---- End of resolve ----
+
+
+        //Due to player input required, these two sections will likely be split into different places in this code as the rework continues!
+
+
+        //---- Start of apply ----
+
+        //select new wave cards
+        // NOT YET IMPLEMENTED
+
+        TemporaryModifiers.CloneValues(PermanentModifiers); //Reset the temporary modifiers to the permanent modifiers
+        //Apply wave cards
+        // NOT YET IMPLEMENTED
+        /* Order
+         * 1. Permanent modifiers
+         * 2. Apply permanent modifiers to temporary modifiers
+         * 3. Apply temporary modifiers
+         */
+
+        //Run current wave card rewards (immediate rewards)
+        // NOT YET IMPLEMENTED
+
+        //---- End of apply ----
+        #endregion
     }
     public static void GatherCredits()
     {
