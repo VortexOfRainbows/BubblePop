87fa239ada46e945c941a3aa495abb321d4ffcb6
Vortex <vortexsnail12@gmail.com>
2025-08-27 01:52:34 -0700

    skull enemy pointers

--- Assets/Resources/NPCs/Champion/BossHealthBar.cs
+++ Assets/Resources/NPCs/Champion/BossHealthBar.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class BossHealthBar : MonoBehaviour
 {
     public static GameObject Prefab => Resources.Load<GameObject>("NPCs/Champion/SkullHealthBar");
-    public SpriteRenderer Inner, Highlight, Skull;
+    public SpriteRenderer Inner, Highlight, Skull, PointerSkull;
     public Enemy Host;
     public static BossHealthBar Spawn(Enemy host)
     {
@@ -17,16 +17,27 @@ public class BossHealthBar : MonoBehaviour
         e.Inner.color = e.Inner.color.WithAlpha(0);
         e.Highlight.color = e.Highlight.color.WithAlpha(0);
         e.Skull.color = e.Skull.color.WithAlpha(0);
+        e.PointerSkull.color = e.PointerSkull.color.WithAlpha(0);
         e.IntendedSize = Mathf.Min(3.5f, host.GetComponent<BoxCollider2D>().size.x * host.transform.localScale.x * 0.5f + 0.55f);
         return e;
     }
     public void Update()
     {
-        UpdateSize();
-        SetAlpha();
+        if(Host != null)
+        {
+            UpdateSize();
+            SetAlpha();
+        }
+        if (!WaveDirector.WaveActive)
+        {
+            UpdatePosition();
+        }
+        else
+            Skull.transform.localPosition = Vector3.zero;
     }
     public float IntendedSize { get; set; }
     public float Alpha { get; set; } = -0.7f;
+    public float PointerSkullAlpha { get; set; } = -0.5f;
     public void SetAlpha()
     {
         if (Alpha < 1)
@@ -53,5 +64,49 @@ public class BossHealthBar : MonoBehaviour
         Vector2 size = new(IntendedSize * percent + 0.55f, 0.4f);
         Inner.size = size;
         Highlight.size = new Vector2(size.x - 0.1f, size.y - 0.15f);
+    }
+    public void UpdatePosition()
+    {
+        Vector2 screenSize = UIManager.ActivePrimaryCanvas.pixelRect.size;
+        Vector2 middle = screenSize / 2;
+        Camera main = Camera.main;
+        Vector2 myPos = main.WorldToScreenPoint(transform.position);
+
+        float PaddingAllowed = 30;
+        float targetDistanceX = screenSize.x / 2 - PaddingAllowed;
+        float targetDistanceY = screenSize.y / 2 - PaddingAllowed;
+        Vector2 toMyPos = myPos - middle;
+        float currentDistanceX = Mathf.Abs(toMyPos.x);
+        float currentDistanceY = Mathf.Abs(toMyPos.y);
+        float ratioX = targetDistanceX / currentDistanceX;
+        float ratioY = targetDistanceY / currentDistanceY;
+        float angle = toMyPos.ToRotation();
+
+        Vector2 relativePosition = toMyPos;
+        if (ratioX < 1 && ratioY < 1)
+        {
+            if(ratioX < ratioY)
+                relativePosition *= ratioX;
+            else
+                relativePosition *= ratioY;
+        }
+        else if (ratioX < 1)
+            relativePosition *= ratioX;
+        else if (ratioY < 1)
+            relativePosition *= ratioY;
+
+        myPos = main.ScreenToWorldPoint(middle + relativePosition);
+
+        PointerSkull.transform.position = new Vector3(myPos.x, myPos.y, PointerSkull.transform.position.z);
+
+        float distanceFromPointer = PointerSkull.transform.localPosition.magnitude;
+        float scaleFactor = Mathf.Clamp((distanceFromPointer - 1), 0, 1);
+
+        if(distanceFromPointer > 1)
+            PointerSkullAlpha += Time.unscaledDeltaTime * 1.5f;
+        else
+            PointerSkullAlpha -= Time.unscaledDeltaTime;
+        PointerSkullAlpha = Mathf.Clamp(PointerSkullAlpha, -0.5f, scaleFactor);
+        PointerSkull.color = PointerSkull.color.WithAlpha(Mathf.Max(PointerSkullAlpha, 0));
     }
 }
