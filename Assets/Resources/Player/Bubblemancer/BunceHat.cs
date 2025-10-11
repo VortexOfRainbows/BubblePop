0a46f0e6421fa50bf30e586fda8fec5d2f5cdbdb
Vortex <vortexsnail12@gmail.com>
2025-10-11 16:28:34 -0700

    bunce hat art refresh!

--- Assets/Resources/Player/Bubblemancer/BunceHat.cs
+++ Assets/Resources/Player/Bubblemancer/BunceHat.cs
@@ -7,12 +7,13 @@ public class BunceHat : BubblemancerHat
     public override void ModifyUIOffsets(bool isBubble, ref Vector2 offset, ref float rotation, ref float scale)
     {
         base.ModifyUIOffsets(isBubble, ref offset, ref rotation, ref scale);
-        scale *= 1.33f;
-        offset.y -= 0.1f;
+        scale *= 1.35f;
+        offset.y -= 0.35f;
+        offset.x += 0.1f;
     }
     public override void InitializeDescription(ref DetailedDescription description)
     {
-        description.WithName("Bunce Cap").WithDescription("Grants an additional heart\n You stupid!");
+        description.WithName("Bunce Cap").WithDescription("Grants an additional heart\nYou stupid!");
     }
     public override void ModifyLifeStats(ref int MaxLife, ref int Life, ref int MaxShield, ref int Shield)
     {
@@ -21,6 +22,7 @@ public class BunceHat : BubblemancerHat
     }
     protected override void AnimationUpdate()
     {
+        float bonus = 0.09f;
         if (player.Body is Bubblemancer)
         {
             if (player.abilityTimer > 0)
@@ -31,11 +33,13 @@ public class BunceHat : BubblemancerHat
             else if (player.AbilityReady)
                 velocity = Vector2.Lerp(velocity, Vector2.zero, 0.15f);
         }
+        else if (player.Body is Gachapon)
+            bonus = 0.12f;
         spriteRender.flipX = !p.Body.Flipped;
         transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(transform.eulerAngles.z, p.MoveDashRotation() - 12 * p.Direction, 0.2f));
         velocity = Vector2.Lerp(velocity, Vector2.zero, 0.2f);
         transform.localPosition = Vector2.Lerp((Vector2)transform.localPosition,
-            new Vector2(-0.1f * p.Direction, (0.2f + 0.5f * p.Bobbing * p.squash - 0.2f * (1 - p.squash))).RotatedBy(transform.eulerAngles.z * Mathf.Deg2Rad),
+            new Vector2(-0.12f * p.Direction, (bonus + 0.37f * p.Bobbing * p.squash - 0.2f * (1 - p.squash))).RotatedBy(transform.eulerAngles.z * Mathf.Deg2Rad),
             0.2f) + velocity;
     }
     protected override void DeathAnimation()
