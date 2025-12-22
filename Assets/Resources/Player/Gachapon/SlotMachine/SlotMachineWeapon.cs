c0235a0cec0f87e01e102e6c848761bfc67c8d1b
Vortex <vortexsnail12@gmail.com>
2025-12-22 01:49:02 -0800

    new patch!

--- Assets/Resources/Player/Gachapon/SlotMachine/SlotMachineWeapon.cs
+++ Assets/Resources/Player/Gachapon/SlotMachine/SlotMachineWeapon.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Tilemaps;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class SlotMachineWeapon : Weapon
 {
