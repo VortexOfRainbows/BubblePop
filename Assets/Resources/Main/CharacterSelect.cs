750baabe2cf24295ed21dddfc7f170f684a6e883
Vortex <vortexsnail12@gmail.com>
2026-02-27 22:42:52 -0800

    changelog and bug fixes

--- Assets/Resources/Main/CharacterSelect.cs
+++ Assets/Resources/Main/CharacterSelect.cs
@@ -185,16 +185,16 @@ public class CharacterSelect : MonoBehaviour
         if (selectMenuOpen)
         {
             prevPosition = HangerButton.transform.position;
-            Utils.LerpSnap(HangerButton.transform, new Vector2(30 - canvasRect.width / 2, 705 - canvasRect.height / 2), lerpFactor);
+            Utils.LerpSnap(HangerButton.transform, new Vector2(30, 705), lerpFactor);
             Utils.LerpSnap(Slider.transform, new Vector2(0, 45), lerpFactor);
-            Utils.LerpSnap(InfoScreen.transform, new Vector2(canvasRect.width / 2 - 30, canvasRect.height / 2 - 140), lerpFactor);
+            Utils.LerpSnap(InfoScreen.transform, new Vector2(-30, -140), lerpFactor);
         }
         else
         {
             prevPosition = HangerButton.transform.position;
-            Utils.LerpSnap(HangerButton.transform, new Vector2(30 - canvasRect.width / 2, 105 - canvasRect.height / 2), lerpFactor);
+            Utils.LerpSnap(HangerButton.transform, new Vector2(30, 105), lerpFactor);
             Utils.LerpSnap(Slider.transform, new Vector2(0, 645), lerpFactor);
-            Utils.LerpSnap(InfoScreen.transform, new Vector2(canvasRect.width / 2 + 430, canvasRect.height / 2 - 140), lerpFactor);
+            Utils.LerpSnap(InfoScreen.transform, new Vector2(430, -140), lerpFactor);
         }
         if (selectMenuOpen)
         {
