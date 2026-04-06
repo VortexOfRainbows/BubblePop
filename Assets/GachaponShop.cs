3c3e00b9f0786f037353aaf42d329b26a312befc
Vortex <13910999+VortexOfRainbows@users.noreply.github.com>
2026-04-06 16:04:18 -0700

    more animations for restock machine

--- Assets/GachaponShop.cs
+++ Assets/GachaponShop.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class GachaponShop : MonoBehaviour
 {
@@ -79,12 +80,23 @@ public class GachaponShop : MonoBehaviour
         float mult = PriceMultiplier * (1.0f + 0.05f * TotalPowersPurchased - Player.Instance.ShopDiscount);
         float bmChance = BlackMarketShop ? 1 : .005f * Player.Instance.BlackmarketMult;
         Vector3 pillowPosition = Pedastal[i].transform.position + new Vector3(0, 1.2f);
-        Vector3 spawnPosition = RestockMachine != null ? RestockMachine.transform.position : pillowPosition;
+        Vector3 spawnPosition = RestockMachine != null ? RestockMachine.transform.position + new Vector3(0, 0.05f) : pillowPosition;
         PowerUpObject obj = PowerUp.Spawn(PowerUp.RandomFromPool(0.05f, bmChance), spawnPosition).GetComponent<PowerUpObject>();
         obj.Cost = Mathf.Max(0, (int)(obj.MyPower.Cost * mult));
         obj.FinalPosition = pillowPosition;
         obj.VelocityStyle = 1;
         obj.velocity = new Vector2(0, 8);
+        if (RestockMachine != null)
+        {
+            AudioManager.PlaySound(SoundID.ChestDrop, spawnPosition, 1, 1);
+            float r = Utils.rand.NextFloat(Mathf.PI * 2);
+            for (int a = 0; a < 30; ++a)
+            {
+                Vector2 circular = new Vector2(1, 0).RotatedBy(Mathf.PI * a / 15f + r);
+                ParticleManager.NewParticle(spawnPosition, Utils.RandFloat(2, 3), circular * Utils.RandFloat(2.5f, 4), 0.2f, Utils.RandFloat(0.7f, 1.5f), ParticleManager.ID.Pixel,
+                    obj.glow.color * 0.7f);
+            }
+        }
         Stock[i] = obj;
     }
 }
--- Assets/Resources/RestockMachine/RestockMachine.cs
+++ Assets/Resources/RestockMachine/RestockMachine.cs
@@ -1,22 +1,18 @@
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
 
 public class RestockMachine : MonoBehaviour
 {
     public SpriteRenderer[] Numbers;
     public SpriteRenderer DisplayNumber;
+    public Transform GumballMachine;
+    public GameObject Smile, OpenSmile;
     public float NumberSwapTimer = 0;
+    public float AnimationTimer = 0;
     public void SetRestockAmountToShopStock(GachaponShop Owner)
     {
-        int index = Owner.RestockRemaining - 1;
-        if(index < 0)
-        {
-            DisplayNumber.sprite = null;
-            return;
-        }
-        if (index >= 10)
-            index = 10;
+        int index = Owner.RestockRemaining;
+        if (index >= Numbers.Length)
+            index = Numbers.Length - 1;
         DisplayNumber.sprite = Numbers[index].sprite;
         DisplayNumber.transform.localPosition = Numbers[index].transform.localPosition;
         DisplayNumber.transform.localScale *= 0;
@@ -29,15 +25,27 @@ public class RestockMachine : MonoBehaviour
     }
     public void Animate()
     {
-
+        AnimationTimer += Time.fixedDeltaTime * 0.8f;
+        float sin = Mathf.Sin(AnimationTimer * Mathf.PI);
+        float sin2 = Mathf.Sin(AnimationTimer * Mathf.PI * 0.5f);
+        float sin3 = Mathf.Sin(NumberSwapTimer * Mathf.PI) * 0.04f;
+        float stretch = sin * 0.025f;
+        GumballMachine.transform.localScale = new Vector3(1 + stretch + sin3, 1 - stretch + sin3, 1);
+        transform.localScale = new Vector3(1 - stretch * 0.3f, 1 + stretch * 0.3f, 1);
+        float tilt = 2.25f * sin2;
+        GumballMachine.transform.SetLocalEulerZ(tilt);
     }
     public void AnimateNumber()
     {
         NumberSwapTimer += Time.fixedDeltaTime * 2.5f;
         if (NumberSwapTimer > 1)
             NumberSwapTimer = 1;
+        //bool smile = NumberSwapTimer > 0 && NumberSwapTimer < 1;
+        //Smile.SetActive(!smile);
+        //OpenSmile.SetActive(smile);
         float sin = Mathf.Sin(NumberSwapTimer * Mathf.PI);
         float scale = sin * sin * 0.5f + NumberSwapTimer;
         DisplayNumber.transform.localScale = Vector3.one * (0.5f + 0.5f * scale);
+        Smile.transform.localScale = OpenSmile.transform.localScale = DisplayNumber.transform.localScale;
     }
 }
