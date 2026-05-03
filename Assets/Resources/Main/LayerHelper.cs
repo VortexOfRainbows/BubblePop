a49d3552c72106169fae593c82909ef53d44c797
Vortex <13910999+VortexOfRainbows@users.noreply.github.com>
2026-05-03 01:54:55 -0700

    major sorting changes

--- Assets/Resources/Main/LayerHelper.cs
+++ Assets/Resources/Main/LayerHelper.cs
@@ -1,43 +1,39 @@
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEngine;
+/// <summary>
+/// THIS CLASS DOESN'T REALLY **DO** ANYTHING
+/// 
+/// It exists as a reminder for which sorting orders are populated by which assets
+/// </summary>
 public static class LayerHelper
 {
-    //This class mostly exists as a reminder for which sorting orders are populated by which assets
-
     //-----------------------------------
     //           FlOOR LAYER (effected by tile shadows)
     //-----------------------------------
 
-    //-------------BACKGROUND------------
     public const int FloorTileSortingOrder = -1;
     public const int WallTileSortingOrder = 0;
-
-
-
-
 
     //-----------------------------------
     //           DEFAULT LAYER (not effected by tile shadows)
     //-----------------------------------
 
-    //-------------BACKGROUND------------
+    public const int ShadowSortingOrder = -50; //Behind everything (shadows)
     public const int FloorObjSortingLayer = -2; //Power Pillows
     public const int PickupSortingOrder = -1; //Coins, Keys, Powers, Etc.
     public const int PortalSortingOrder = -1;
     public const int TrailSortingOrder = -1;
+    public const int ShopGachaponSortingOrder = -1;
+    public const int ChestSortingOrder = -1;
 
-    //-------------FOREGROUND------------
     public const int PlayerEnemyWorldObj = 0;
     public const int FloraSortingOrder = 0; //All ground decor
+    public const int CrucibleSortingOrder = 0;
     public const int SolidTileSortingOrder = 1;
+    public const int RestockMachineTopSortingOrder = 1; //Restock Machine (TOP)
     public const int SpecialHatSortingOrder = 2; //Bulb, Crown
     public const int WeaponSortingOrder = 3;
 
-
     //-----------------------------------
-    //       Player Sorting Group
+    //       Player Sorting Group (sublayers)
     //-----------------------------------
     public const int CapeBack = -3;
     public const int Skateboard = -2;
@@ -51,7 +47,6 @@ public static class LayerHelper
     //          DEFAULT LAYER (not yet integrated)
     //-----------------------------------
     public const int Projectile = 5;
-    public const int ShadowSortingOrder = -50;
     public const int InWorldUIElement = 10; //Boss/Skull health bars
     public const int ArbitraryCompendiumUISpriteMask = 30;
 }
