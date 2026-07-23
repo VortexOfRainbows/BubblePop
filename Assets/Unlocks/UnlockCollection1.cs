210a8d3a3f6d875b1a1937782933eb0d6ba3f4dc
Vortex <vortexsnail12@gmail.com>
2026-07-23 05:22:20 -0700

    import assets and lots of notes

--- Assets/Unlocks/UnlockCollection1.cs
+++ Assets/Unlocks/UnlockCollection1.cs
@@ -278,5 +278,10 @@ public class FizzyStuffed : UnlockCondition
     public override int Rarity => 4;
     public override UnlockCondition PreReqUnlock => Get<FizzyUnlock>();
 }
-//Achievement idea for Oil King: Quagmire: afflict an enemy with tar, chill, and poison
-//Achievement idea for Oil King: Environmentalist: win without afflicting tar on any enemy
+//Achievement idea for (2-star) Fizzy: Speedrun: As Fizzy, win in under 20 minutes (does not count paused time)
+//Achievement idea for (3-star) Fizzy: Touch Grass: As Fizzy, win while holding Rainbow Flower, Pity Charm, and Lucky Star
+//Achievement idea for (5-star) Fizzy: Third Day: As Fizzy, reach the third loop on Ascension 3 without dying, then resurrect
+//Achievement idea for (2-star) Oil King: Rock Feller: as Oil King, kill all segments of a Rock Golem with a single drone strike
+//Achievement idea for (3-star) Oil King: Quagmire: afflict an enemy with tar, chill, and poison, then detonate them for over 1000 total damage from debuffs
+//Achievement idea for (4-star) Oil King: Environmentalist: win without afflicting tar on any enemy
+//Achievement idea for (5-star) Oil King: Golden Slaughterer: win on Ascension 3 without taking damage, picking up red items, or taking non-investments from choices
